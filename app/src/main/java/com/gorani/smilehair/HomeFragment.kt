3ce249914c55eeba1adad9d7168ff06ca4d21ec7
gorani16 <jgee0116@naver.com>
2022-09-15 20:27:10 +0900

    1. JSON Format Data 생성 2. Asset Loader 구현 완료 3. JSON Data 파싱 4. Glide 라이브러리 연동 완료 및 인터넷 권한 설정 완료

--- app/src/main/java/com/gorani/smilehair/HomeFragment.kt
+++ app/src/main/java/com/gorani/smilehair/HomeFragment.kt
@@ -1,13 +1,17 @@
 package com.gorani.smilehair
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.json.JSONObject
 
 class HomeFragment: Fragment() {
 
@@ -22,10 +26,27 @@ class HomeFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.btn_example_fragment)
-        button.setOnClickListener {
-            Toast.makeText(context, "Home Fragment Button!", Toast.LENGTH_SHORT).show()
-            findNavController().navigate(R.id.action_home_to_example)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+
+        val assetLoader = AssetLoader()
+        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeData ?: "")
+
+        if (!homeData.isNullOrEmpty()) {
+            val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl = title.getString("icon_url")
+
+            toolbarTitle.text = text
+            GlideApp.with(this)
+                .load(iconUrl)
+                .into(toolbarIcon)
+
+
+
         }
+
     }
 }
---
+++ app/src/main/java/com/gorani/smilehair/Title.kt
@@ -0,0 +1,6 @@
+package com.gorani.smilehair
+
+data class Title(
+    val iconUrl: String,
+    val text: String
+)
